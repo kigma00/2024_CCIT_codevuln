3b51958183ebb859411717291023549c359ff6b8
shyun020 <127273427+shyun020@users.noreply.github.com>
2024-04-16 16:32:07 +0900

    Update query-setting.sh

--- Code_Threading/Threading_v4.0/query-setting.sh
+++ Code_Threading/Threading_v4.0/query-setting.sh
@@ -14,12 +14,13 @@ echo""
 # query setting
 echo -e "\033[32m[+] Git clone\033[0m $@"
 read -p "Enter git clone address : " repository_url
+directory_name=$(basename "$repository_url")
 clone_directory_name="$directory_name"-repo
 mkdir -p /home/codevuln/target-repo/$directory_name/$clone_directory_name   
 echo -e "\033[32m[+] git clone : /home/codevuln/target-repo/$directory_name/$clone_directory_name\033[0m $@"
 git clone --depth=1 "$repository_url" "/home/codevuln/target-repo/$directory_name/$clone_directory_name"
 
-directory_name=$(basename "$repository_url")
+
 
 mkdir "/home/codevuln/target-repo/$directory_name"
 mkdir "/home/codevuln/target-repo/$directory_name/codeql"
