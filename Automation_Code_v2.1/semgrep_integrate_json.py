3b6d915ca1e6c5945e8533b3e8a62f768dc65ec8
J1vvoo <128969316+J1vvoo@users.noreply.github.com>
2024-04-26 00:54:19 +0900

    Create semgrep_integrate_json.py

---
+++ Automation_Code_v2.1/semgrep_integrate_json.py
@@ -0,0 +1,21 @@
+import os
+import json
+import subprocess
+
+directory = f"/home/codevuln/target-repo/{directory_name}/semgrep"
+output_filename = "semgrep.json"
+data = {}
+
+for i, filename in enumerate(os.listdir(directory), start=1):
+    if filename.endswith('.json'):
+        file_path = os.path.join(directory, filename)
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+            data[f"{filename}"] = data
+
+with open(os.path.join(directory, output_filename), 'w') as f:
+    json.dump(data, f, indent=4)
+
+print(f"All JSON files have been merged into {output_filename} in the directory {directory}.")
+
+subprocess.run(['jq', '.', 'semgrep.json'], stdout=open('semgrep.json', 'w'))
