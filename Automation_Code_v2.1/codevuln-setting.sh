bff5d56507585166c175fcf9a890cae9e0c0fe44
shyun020 <127273427+shyun020@users.noreply.github.com>
2024-04-25 12:01:32 +0900

    Update codevuln-setting.sh

--- Automation_Code_v2.1/codevuln-setting.sh
+++ Automation_Code_v2.1/codevuln-setting.sh
@@ -67,33 +67,97 @@ codevuln_setting() {
     echo -e "\033[32m[+] Semgrep Install & Setting\033[0m $@"
     cd /home/codevuln/semgrep/
     pip install semgrep
-    read -r -d '' python_code << 'EOF'
+    read -r -d '' python_code_01 << 'EOF'
 import json
 import csv
 
-with open('results.json') as f:
+with open('results.json', 'r') as f:
     data = json.load(f)
 
 with open('result.csv', 'w', newline='') as csvfile:
-    fieldnames = ['check_id', 'path', 'start_line', 'end_line', 'message']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    metadata_fields = set()
 
+    for result in data['results']:
+        if 'extra' in result:
+            metadata_fields.update(result['extra'].keys())
+
+    fieldnames = ['path', 'start_col', 'start_line', 'start_offset',
+                  'end_col', 'end_line', 'end_offset', 'message', 'severity', 'rule_id']
+    fieldnames += sorted(list(metadata_fields))
+
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
     for result in data['results']:
-        writer.writerow({
-            'check_id': result['check_id'],
-            'path': result['path'],
-            'start_line': result['start']['line'],
-            'end_line': result['end']['line'],
-            'message': result['extra']['message']
-        })
-EOF
-    echo "$python_code" > /home/codevuln/semgrep/json_csv.py
-    chmod +x /home/codevuln/semgrep/json_csv.py
-    echo -e "\033[32m[README] Please copy the URL and login from the browser\033[0m $@"
-    echo -e "\033[32m[+] Semgrep Install & Setting complete\033[0m $@"
+        row = {
+            'path': result.get('path', ''),
+            'start_col': result.get('start', {}).get('col', ''),
+            'start_line': result.get('start', {}).get('line', ''),
+            'start_offset': result.get('start', {}).get('offset', ''),
+            'end_col': result.get('end', {}).get('col', ''),
+            'end_line': result.get('end', {}).get('line', ''),
+            'end_offset': result.get('end', {}).get('offset', ''),
+            'message': result.get('message', ''),
+            'severity': result.get('severity', ''),
+            'rule_id': result.get('check_id', '')
+        }
 
+        if 'extra' in result:
+            for field in metadata_fields:
+                row[field] = result['extra'].get(field, '')
+
+        writer.writerow(row)
+
+EOF
+    echo "$python_code_01" > /home/codevuln/semgrep/json_csv.py
+    chmod +x /home/codevuln/semgrep/json_csv.py
+
+    read -r -d '' python_code_02 << 'EOF'
+import os
+import pandas as pd
+import sys
+import subprocess
+from datetime import datetime
+
+def integrate_csv_files(directory_path, output_file):
+    frames = []
+    for file in os.listdir(directory_path):
+        if file.endswith('.csv') and file != "semgrep.csv":  
+            file_path = os.path.join(directory_path, file)
+            if os.path.getsize(file_path) > 0:  
+                df = pd.read_csv(file_path, engine='python')  
+            else:
+                print(f"Skipped empty file: {file_path}")
+    if frames:  
+        result = pd.concat(frames, ignore_index=True)  
+        
+        today_date = datetime.now().strftime('%Y-%m-%d')
+        result.insert(0, 'date', today_date)
+        
+        current_time = datetime.now().strftime('%H:%M:%S')
+        result.insert(1, 'time', current_time)
+        
+        result.to_csv(output_file, index=False) 
+        print(f"Output saved to {output_file}")
+    else:
+        print("No data to process.")
+
+    delete_command = f"mv {directory_path}semgrep.csv /home/codevuln/ && rm -rf {directory_path}*.csv && mv /home/codevuln/semgrep.csv {directory_path} && exit"
+    subprocess.run(delete_command, shell=True, check=True, cwd=directory_path)  
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        raise ValueError("This script requires exactly two arguments: the directory path and the output file path.")
+
+    directory_path = sys.argv[1]  
+    output_file = sys.argv[2]  
+
+    integrate_csv_files(directory_path, output_file)
+EOF
+    echo "$python_code_02" > /home/codevuln/semgrep/integrate_csv_files.py
+    chmod +x /home/codevuln/semgrep/integrate_csv_files.py
+    echo -e "\033[32m[+] Semgrep Install & Setting complete\033[0m $@"
 
     echo -e "\033[32m[+] SonarQube Install & Setting\033[0m $@"
     cd /home/codevuln/sonarqube
@@ -131,9 +195,12 @@ EOF
         echo "SonarScanner is already installed."
     fi
     echo -e "\033[32m[+] SonarQube Install & Setting complete\033[0m $@"
+
+    echo -e "\033[32m[README] Please copy the URL and login from the browser\033[0m $@"
+    semgrep login
 }
 
 clear
-sleep 2
 banner
+sleep 2
 codevuln_setting
