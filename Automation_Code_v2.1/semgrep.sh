a2a0bebfc1e4a31bbc178453a00cc6f165db82f3
shyun020 <127273427+shyun020@users.noreply.github.com>
2024-04-25 12:01:52 +0900

    Update semgrep.sh

--- Automation_Code_v2.1/semgrep.sh
+++ Automation_Code_v2.1/semgrep.sh
@@ -1,125 +1,32 @@
 #!/bin/bash
-
 directory_name=$1
 clone_directory_name=$2
 
-clear
-echo -e "\033[32m[RUN] Semgrep default scan\033[0m $@"
-cd /home/codevuln/target-repo/$directory_name/semgrep/
-cp /home/codevuln/semgrep/json_csv.py ./
-sleep 1
-semgrep --config=p/default /home/codevuln/target-repo/$directory_name/$clone_directory_name --json > ./results.json
-sleep 1
-python3 ./json_csv.py
-mv ./result.csv ./default.csv
-rm -rf ./results.json
-rm -rf ./json_csv.py
-sleep 2
+base_dir="/home/codevuln/target-repo/$directory_name/semgrep"
+clone_path="/home/codevuln/target-repo/$directory_name/$clone_directory_name"
 
-echo -e "\033[32m[RUN] Semgrep cwe-top-25 scan\033[0m $@"
-cd /home/codevuln/target-repo/$directory_name/semgrep
-cp /home/codevuln/semgrep/json_csv.py ./
-sleep 2            
-semgrep --config=p/cwe-top-25 /home/codevuln/target-repo/$directory_name/$clone_directory_name --json > ./results.json
-sleep 2
-python3 ./json_csv.py
-mv ./result.csv ./cwe-top-25.csv
-rm -rf ./results.json
-rm -rf ./$clone_directory_name/json_csv.py        
-sleep 2
+run_semgrep_scan () {
+  local config=$1         
+  local output_prefix=$2 
+  echo -e "\033[32m[RUN] Semgrep $output_prefix scan\033[0m"
+  cd $base_dir || exit  
+  cp /home/codevuln/semgrep/json_csv.py ./  
+  
+  semgrep --config=p/$config $clone_path --json > ./results.json  
+  python3 ./json_csv.py 
+  mv ./result.csv ./$output_prefix.csv  
+  mv ./results.json ./$output_prefix.json  
+  rm -f ./json_csv.py  
+}
 
-echo -e "\033[32m[RUN] Semgrep owasp-top-ten scan\033[0m $@"
-cd /home/codevuln/target-repo/$directory_name/semgrep
-cp /home/codevuln/semgrep/json_csv.py ./
-sleep 2            
-semgrep --config=p/owasp-top-ten /home/codevuln/target-repo/$directory_name/$clone_directory_name --json > ./results.json
-sleep 2
-python3 ./json_csv.py
-mv ./result.csv ./owasp-top-ten.csv
-rm -rf ./results.json
-rm -rf ./$clone_directory_name/json_csv.py
-sleep 2
+declare -A scans=(
+  ["default"]="default"
+  ["cwe-top-25"]="cwe-top-25"
 
-echo -e "\033[32m[RUN] Semgrep r2c-security-audit scan\033[0m $@"
-cd /home/codevuln/target-repo/$directory_name/semgrep
-cp /home/codevuln/semgrep/json_csv.py ./
-sleep 2            
-semgrep --config=p/r2c-security-audit /home/codevuln/target-repo/$directory_name/$clone_directory_name --json > ./results.json
-sleep 2
-python3 ./json_csv.py
-mv ./result.csv ./r2c-security-audit.csv
-rm -rf ./results.json
-rm -rf ./$clone_directory_name/json_csv.py
-sleep 2
+)
 
-echo -e "\033[32m[RUN] Semgrep command-injection scan\033[0m $@"
-cd /home/codevuln/target-repo/$directory_name/semgrep
-cp /home/codevuln/semgrep/json_csv.py ./
-sleep 2            
-semgrep --config=p/command-injection /home/codevuln/target-repo/$directory_name/$clone_directory_name --json > ./results.json
-sleep 2
-python3 ./json_csv.py
-mv ./result.csv ./command-injection.csv
-rm -rf ./results.json
-rm -rf ./$clone_directory_name/json_csv.py
-sleep 2       
+for config in "${!scans[@]}"; do
+  run_semgrep_scan $config ${scans[$config]}
+done
 
-echo -e "\033[32m[RUN] Semgrep insecure-transport scan\033[0m $@"
-cd /home/codevuln/target-repo/$directory_name/semgrep
-cp /home/codevuln/semgrep/json_csv.py ./
-sleep 2            
-semgrep --config=p/insecure-transport /home/codevuln/target-repo/$directory_name/$clone_directory_name --json > ./results.json
-sleep 2
-python3 ./json_csv.py
-mv ./result.csv ./insecure-transport.csv
-rm -rf ./results.json
-rm -rf ./$clone_directory_name/json_csv.py
-sleep 2                           
-
-echo -e "\033[32m[RUN] Semgrep jwt scan\033[0m $@"
-cd /home/codevuln/target-repo/$directory_name/semgrep
-cp /home/codevuln/semgrep/json_csv.py ./
-sleep 2            
-semgrep --config=p/jwt /home/codevuln/target-repo/$directory_name/$clone_directory_name --json > ./results.json
-sleep 2
-python3 ./json_csv.py
-mv ./result.csv ./jwt.csv
-rm -rf ./results.json
-rm -rf ./$clone_directory_name/json_csv.py
-sleep 2
-
-echo -e "\033[32m[RUN] Semgrep secrets scan\033[0m $@"
-cd /home/codevuln/target-repo/$directory_name/semgrep
-cp /home/codevuln/semgrep/json_csv.py ./
-sleep 2            
-semgrep --config=p/secrets /home/codevuln/target-repo/$directory_name/$clone_directory_name --json > ./results.json
-sleep 2
-python3 ./json_csv.py
-mv ./result.csv ./secrets.csv
-rm -rf ./results.json
-rm -rf ./$clone_directory_name/json_csv.py
-sleep 2
-
-echo -e "\033[32m[RUN] Semgrep sql-injection scan\033[0m $@"
-cd /home/codevuln/target-repo/$directory_name/semgrep
-cp /home/codevuln/semgrep/json_csv.py ./
-sleep 2            
-semgrep --config=p/sql-injection /home/codevuln/target-repo/$directory_name/$clone_directory_name --json > ./results.json
-sleep 2
-python3 ./json_csv.py
-mv ./result.csv ./sql-injection.csv
-rm -rf ./results.json
-rm -rf ./$clone_directory_name/json_csv.py
-sleep 2
-
-echo -e "\033[32m[RUN] Semgrep xss scan\033[0m $@"
-cd /home/codevuln/target-repo/$directory_name/semgrep
-cp /home/codevuln/semgrep/json_csv.py ./
-sleep 2            
-semgrep --config=p/xss /home/codevuln/target-repo/$directory_name/$clone_directory_name --json > ./results.json
-sleep 2
-python3 ./json_csv.py
-mv ./result.csv ./xss.csv
-rm -rf ./results.json
-rm -rf ./$clone_directory_name/json_csv.py
-sleep 2
+python3 /home/codevuln/semgrep/integrate_csv_files.py "/home/codevuln/target-repo/$directory_name/semgrep" "/home/codevuln/target-repo/$directory_name/semgrep/semgrep.csv"
