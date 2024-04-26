de2aef4ae9c337cf520d572b475eb89ef24fc7b7
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2024-04-26 14:42:07 +0900

    Update codeql.sh

--- Automation_Code_v2.1/codeql.sh
+++ Automation_Code_v2.1/codeql.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# 환경 설정 소스
+source ~/.bashrc
+        
+# CodeQL 분석을 CSV로 실행
+cd ~
+source ./.bashrc
+
+export 
+
 directory_name=$1
 clone_directory_name=$2
 language=$3
@@ -7,7 +16,7 @@ language=$3
 echo -e "\033[32m[+] Create database\033[0m $@"
 sleep 2
 cd ~
-source ./.bashrc
+
 codeql database create --language="$language" --source-root="/home/codevuln/target-repo/$directory_name/$clone_directory_name" "/home/codevuln/target-repo/$directory_name/codeql/codeql-db-$directory_name"
 
 cwe_directories=$(find /home/codevuln/codeql/codeql-repo/$language/ql/src/Security/ -type d -name "CWE*")
@@ -20,12 +29,7 @@ for dir in $cwe_directories; do
         # CSV 파일 경로 생성
         csv_output_file="/home/codevuln/target-repo/$directory_name/codeql/$(basename ${ql_file%.ql}).csv"
         
-        # 환경 설정 소스
-        source ~/.bashrc
         
-        # CodeQL 분석을 CSV로 실행
-        cd ~
-        source ./.bashrc
 
         
         codeql database analyze "/home/codevuln/target-repo/$directory_name/codeql/codeql-db-$directory_name" "$ql_file" --format=csv --output="$csv_output_file"
