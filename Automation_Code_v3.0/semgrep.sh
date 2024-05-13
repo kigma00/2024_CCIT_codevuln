1f0e4f78f5cac0d02b0491d976e1c33f1e41edc8
J1vvoo <128969316+J1vvoo@users.noreply.github.com>
2024-05-13 15:16:16 +0900

    Create semgrep.sh

---
+++ Automation_Code_v3.0/semgrep.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+directory_name=$1
+clone_directory_name=$2
+
+base_dir="/home/codevuln/target-repo/$directory_name/semgrep"
+clone_path="/home/codevuln/target-repo/$directory_name/$clone_directory_name"
+
+run_semgrep_scan () {
+  local config=$1         
+  local output_prefix=$2 
+  echo -e "\033[32m[RUN] Semgrep $output_prefix scan\033[0m"
+  cd $base_dir || exit  
+  cp /home/codevuln/semgrep/semgrep_json_csv.py ./  
+  
+  semgrep --config=p/$config $clone_path --json > ./results.json  
+  python3 ./semgrep_json_csv.py
+  mv ./result.csv ./$output_prefix.csv  
+  mv ./results.json ./$output_prefix.json  
+  rm -f ./semgrep_json_csv.py
+}
+
+declare -A scans=(
+  ["default"]="default"
+  ["cwe-top-25"]="cwe-top-25"
+  ["owasp-top-ten"]="owasp-top-ten"
+  ["r2c-security-audit"]="r2c-security-audit"
+  ["command-injection"]="command-injection"
+  ["insecure-transport"]="insecure-transport"
+  ["jwt"]="jwt"
+  ["secrets"]="secrets"
+  ["sql-injection"]="sql-injection"
+  ["xss"]="xss"
+)
+
+for config in "${!scans[@]}"; do
+  run_semgrep_scan $config ${scans[$config]}
+done
+
+python3 /home/codevuln/semgrep/semgrep_integrate_csv.py "/home/codevuln/target-repo/$directory_name/semgrep" "/home/codevuln/target-repo/$directory_name/semgrep/semgrep.csv"
+
+cp /home/codevuln/semgrep/semgrep_column_delete.py $base_dir
+python3 semgrep_column_delete.py
+rm -rf ./semgrep_column_delete.py
+
+cp /home/codevuln/semgrep/semgrep_column_order.py $base_dir
+python3 semgrep_column_order.py
+rm -rf ./semgrep_column_order.py
+
+cd /home/codevuln/target-repo/$directory_name/semgrep
+jq -s '[.[][]]' ./default.json ./owasp-top-ten.json ./r2c-security-audit.json ./cwe-top-25.json ./owasp-top-ten.json ./r2c-security-audit.json ./command-injection.json ./insecure-transport.json ./jwt.json ./secrets.json ./sql-injection.json ./xss.json > semgrep.json
+mv semgrep.json ..
+rm -f ./*.json
+mv ../semgrep.json ./
+
+mv /home/codevuln/target-repo/${directory_name}/semgrep/semgrep.csv /home/codevuln/target-repo/${directory_name}/scan_result
+mv /home/codevuln/target-repo/${directory_name}/semgrep/semgrep.json /home/codevuln/target-repo/${directory_name}/scan_result
+rm -rf /home/codevuln/target-repo/${directory_name}/semgrep
+
+exit 0
