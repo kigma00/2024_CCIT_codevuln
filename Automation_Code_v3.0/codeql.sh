421f6efcfadbf7633a59aee631b8f55b161cd83e
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2024-05-17 04:23:02 +0900

    Update codeql.sh

--- Automation_Code_v3.0/codeql.sh
+++ Automation_Code_v3.0/codeql.sh
@@ -20,7 +20,7 @@ cwe_directories="/home/codevuln/codeql/codeql-repo/$language/ql/src/Security"
 #codeql database finalize /home/codevuln/target-repo/mongo/codeql/codeql-db-mongo
 
 # CSV 파일 경로 생성
-csv_output_file="/home/codevuln/target-repo/$directory_name/codeql/($directory).csv"
+csv_output_file="/home/codevuln/target-repo/$directory_name/codeql/($directory_name).csv"
 
 codeql database analyze "/home/codevuln/target-repo/$directory_name/codeql/codeql-db-$directory_name" "$cwe_directories" --format=csv --output="$csv_output_file"
 
