47678ba86c47c76af244ea9baeb956422619367c
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2024-05-20 21:26:53 +0900

    Update codeql.sh

--- Automation_Code_v3.0/codeql.sh
+++ Automation_Code_v3.0/codeql.sh
@@ -2,7 +2,6 @@
 
 # 환경 설정 소스
 #source ~/.bashrc
-#export 
 
 directory_name=$1
 clone_directory_name=$2
@@ -24,19 +23,7 @@ csv_output_file="/home/codevuln/target-repo/$directory_name/codeql/codeql.csv"
 
 codeql database analyze "/home/codevuln/target-repo/$directory_name/codeql/codeql-db-$directory_name" "$cwe_directories" --format=csv --output="$csv_output_file"
 
-#cwe_directories=$(find /home/codevuln/codeql/codeql-repo/$language/ql/src/Security/ -type d -name "CWE*")
-#for dir in $cwe_directories; do
-#    ql_files=$(find "$dir" -type f -name "*.ql")
-#    for ql_file in $ql_files; do
-#        echo "Analyzing $ql_file..."
-         # CSV 파일 경로 생성
-#        csv_output_file="/home/codevuln/target-repo/$directory_name/codeql/$(basename ${ql_file%.ql}).csv"
-#        codeql database analyze "/home/codevuln/target-repo/$directory_name/codeql/codeql-db-$directory_name" "$ql_file" --format=csv --output="$csv_output_file"
-#        echo "CSV output saved to $csv_output_file"
-#    done
-#done
-
 # Python 스크립트를 호출하여 CSV 파일에 헤더를 추가하고 파일을 통합
-python3 /home/codevuln/codeql/codeql_integrate_csv.py "$directory_name" "$clone_directory_name"
+# python3 /home/codevuln/codeql/codeql_integrate_csv.py "$directory_name" "$clone_directory_name"
 
 echo "Python script executed successfully."
