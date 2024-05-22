eacadcd42b6ec7944f139d0cafc8070f6eab2de1
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2024-05-22 19:51:46 +0900

    Update codeql.sh

--- Automation_Code_v3.0/codeql.sh
+++ Automation_Code_v3.0/codeql.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# 환경 설정 소스
-#source ~/.bashrc
-
 directory_name=$1
 clone_directory_name=$2
 language=$3
+date=$(date +"%y%m%d")
+time=$(date +"%H%M%S")
 
 echo -e "\033[32m[+] Create database\033[0m $@"
 sleep 2
@@ -15,11 +14,8 @@ codeql database create --language="$language" --source-root="/home/codevuln/targ
 
 cwe_directories="/home/codevuln/codeql/codeql-repo/$language/ql/src/Security"
 
-#test
-#codeql database finalize /home/codevuln/target-repo/mongo/codeql/codeql-db-mongo
-
 # CSV 파일 경로 생성
-csv_output_file="/home/codevuln/target-repo/$directory_name/codeql/codeql.csv"
+csv_output_file="/home/codevuln/scan_result/$date"_"$time"_"$directory_name/$d:irectory_name.csv"
 
 codeql database analyze "/home/codevuln/target-repo/$directory_name/codeql/codeql-db-$directory_name" "$cwe_directories" --format=csv --output="$csv_output_file"
 
