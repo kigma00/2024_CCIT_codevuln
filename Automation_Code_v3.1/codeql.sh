1ababf0f127b515fc99c589412adebf25970beb4
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2024-05-28 01:32:22 +0900

    Update codeql.sh

--- Automation_Code_v3.1/codeql.sh
+++ Automation_Code_v3.1/codeql.sh
@@ -15,11 +15,18 @@ codeql database create --language="$language" --source-root="/home/codevuln/targ
 cwe_directories="/home/codevuln/codeql/codeql-repo/$language/ql/src/Security"
 
 # CSV 파일 경로 생성
-csv_output_file="/home/codevuln/scan_result/$DATE"_"$TIME"_"$directory_name/codeql.csv"
+csv_output_dir="/home/codevuln/scan_result/$DATE"_"$TIME"_"$directory_name"
+csv_output_file="$csv_output_dir/codeql.csv"
+
+# 디렉토리가 존재하지 않으면 생성
+mkdir -p "$csv_output_dir"
+
+# 지정된 파일 삭제
+rm -rf /home/codevuln/codeql/codeql-repo/javascript/ql/src/Security/CWE-020/ExternalAPIsUsedWithUntrustedData.ql
 
 codeql database analyze "/home/codevuln/target-repo/$directory_name/codeql/codeql-db-$directory_name" "$cwe_directories" --format=csv --output="$csv_output_file"
 
 # Python 스크립트를 호출하여 CSV 파일에 헤더를 추가하고 파일을 통합
-python3 /home/codevuln/codeql/codeql_integrate_csv.py "$directory_name" "$clone_directory_name" "$DATE" "$TIME"
+# python3 /home/codevuln/codeql/codeql_integrate_csv.py "$directory_name" "$clone_directory_name" "$DATE" "$TIME"
 
 echo "Python script executed successfully."
