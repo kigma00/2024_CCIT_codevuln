b786b263bd14e922218efa7f88e8546e8bdb21db
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2024-06-15 15:59:31 +0900

    Update codevuln-setting.sh

--- Automation_Code_v3.1/codevuln-setting.sh
+++ Automation_Code_v3.1/codevuln-setting.sh
@@ -76,13 +76,12 @@ codevuln_setting() {
     chmod +x ./semgrep_column_order.py && mv ./semgrep_column_order.py /home/codevuln/semgrep
     chmod +x ./sonarqube-query-action.py #&& mv ./sonarqube-query-action.py /home/codevuln/sonarqube
 
-
     echo -e "\033[32m[+] codeQL Install & Setting\033[0m $@"
     cd /home/codevuln/codeql
     apt install git -y
     git clone https://github.com/github/codeql /home/codevuln/codeql/codeql-repo
     echo -e "\033[32m[+] codeql repo download complete\033[0m $@"
-
+    
     wget https://github.com/github/codeql-cli-binaries/releases/download/v2.17.2/codeql-linux64.zip
     unzip codeql-linux64.zip
     mv ./codeql ./codeql-cli
@@ -92,12 +91,12 @@ codevuln_setting() {
     source ~/.bashrc
     source ./.bashrc
     echo -e "\033[32m[+] codeQL Install & Setting complete\033[0m $@"
-
+    
     echo -e "\033[32m[+] Semgrep Install & Setting\033[0m $@"
     cd /home/codevuln/semgrep/
     pip install semgrep
     echo -e "\033[32m[+] Semgrep Install & Setting complete\033[0m $@"
-
+    
     echo -e "\033[32m[+] SonarQube Install & Setting\033[0m $@"
     cd /home/codevuln/sonarqube
     # SonarQube 및 SonarScanner가 설치되어 있는지 확인
@@ -143,18 +142,17 @@ password = "admin"
 
 sonar = SonarQubeClient(sonarqube_url=url, username=username, password=password)
 END
-
     echo -e "\033[32m[+] SonarQube Install & Setting complete\033[0m $@"
-
     echo -e "\033[32m[README] Please copy the URL and login from the browser\033[0m $@"
 }
 
 clear
 banner
 sleep 2
-codevuln_setting
 
+codevuln_setting
 sleep 2
+
 target_directory="/home/codevuln"
 files=("codevuln-setting.sh" "query-setting.sh" "codeql.sh" "semgrep.sh" "sonarqube.sh")
 
