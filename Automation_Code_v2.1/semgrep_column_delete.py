594538c8904fbd6064dcb693be57dfcbf6d7f980
J1vvoo <128969316+J1vvoo@users.noreply.github.com>
2024-06-20 12:55:53 +0900

    Delete Automation_Code_v2.1 directory

--- Automation_Code_v2.1/semgrep_column_delete.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('semgrep.csv')
-
-columns_to_drop = ['dataflow_trace', 'engine_kind', 'fingerprint', 'is_ignored', 'validation_state', 'metavars']
-
-df.drop(columns=columns_to_drop, inplace=True)
-
-df.to_csv('semgrep.csv', index=False)
